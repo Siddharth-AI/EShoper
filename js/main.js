cfe673ac036229e7c52c9c9d850f44780b49fa44
Siddharth <47350378+Siddharth-AI@users.noreply.github.com>
2024-09-22 08:17:58 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,45 @@
+
+$(document).ready(function () {
+  $('.section-hero--slides').slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+  $('.section-brand--slides').slick({
+    arrows: false,
+    infinite: true,
+    speed: 1200,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 6,
+    slidesToScroll: 6,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          infinite: true,
+
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+});
+
